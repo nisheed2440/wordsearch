2e09684ab14d6e9064f03d9f9101e8732f351c3a
Nisheed Jagadish <njagadish3@sapient.com>
2016-09-15 02:31:54 +0530

    Working service worker

--- src/app.js
+++ src/app.js
@@ -1,10 +1,4 @@
 import WordSearch from './scripts/wordsearch';
 import './styles/app.scss';
 
-
-if('serviceWorker' in window.navigator) {
-    window.navigator.serviceWorker.register('service-worker.js');
-}
-
-
 new WordSearch(document.getElementById('frame'),window.wordsearch);
